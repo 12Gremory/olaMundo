d51162b48f0be99cf5caed6d9ccbf08f56c23027
Thomas <thomas0369@hotmail.com>
2023-09-30 00:47:12 -0300

    adição do footer e cards da página inicial

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,12 +1,19 @@
 import Banner from "Componentes/Banner"
+import styles from "./inicio.module.css"
+import posts from "json/posts.json"
+import Post from "Componentes/Post"
 
 function Inicio () {
     return (
         <main>
             <Banner/>
-            <h1>
-                Olá mundo!
-            </h1>
+            <ul className={styles.posts}>
+                {posts.map((post) => (
+                    <li key={post.id}>
+                        <Post post={post}/>
+                    </li>
+                ))}
+            </ul>
         </main>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./Componentes/Menu";
+import Rodape from "Componentes/Rodape";
 
 
 function AppRoutes() {
@@ -13,6 +14,7 @@ function AppRoutes() {
         <Route path="/sobremim" element={<SobreMim/>}/>
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
+      <Rodape/>
     </BrowserRouter>
   );
 }
